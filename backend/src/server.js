b8e8e26c96ceabb1d7d2d829d10d7b360647d4a3
issahar987 <issahar987@gmail.com>
2022-04-04 09:35:49 +0200

    fix: add backend with db, lock.json may not work and cause conflicts

---
+++ backend/src/server.js
@@ -0,0 +1,46 @@
+// Definicja modułów
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const sequelize = require('../db/database');
+const Client = require('../db/Client');
+
+sequelize.sync().then(() => console.log('db is ready'));
+
+const app = express();
+
+// Wykorzystywane rozszerzenia
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Informacja przy wejsciu na adres serwera
+app.get('/', (reqName, resName) => {
+  console.log(reqName);
+  resName.send('Serwer XevaApp działa!');
+});
+
+// API dla formularza dla klienta
+app.post('/clients', async (req, res) => {
+  console.log('body request', req.body);
+
+  await Client.create(req.body);
+  res.send('Client is insreted in DB');
+});
+
+app.get('/clients', async (req, res) => {
+  const clients = await Client.findAll();
+  res.send(clients);
+});
+
+app.get('/clients/:id', async (req, res) => {
+  const reqID = req.params.id;
+  const client = await Client.findOne({ where: { id: reqID } });
+  res.send(client);
+});
+
+// Ustawienie portu nasłuchiwania serwera
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Serwer wystartował na porcie ${PORT}.`);
+});
