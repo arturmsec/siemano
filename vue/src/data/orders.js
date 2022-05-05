d802ca1741d07f5b211557d1766b40d845befe22
mmichalm <zgredek2318@gmail.com>
2022-05-05 18:51:35 +0200

    Order display from file

---
+++ vue/src/data/orders.js
@@ -0,0 +1,60 @@
+const orders = [
+    {
+        id: 1,
+        priority: "Normal",
+        orderType: "Pomiar rolety",
+        address: "Wyb. Wyspiańskiego 27",
+        city: "Wrocław",
+        postCode: "50-370",
+        name: "Małgorzata Walernowicz",
+        nrtel: "500 500 500",
+        date: "01-01-2000",
+        description: "Roleta pionowa i 2 drewniane, telefon po godz. 17:00"
+
+
+
+    },
+    {
+        id: 2,
+        priority: "Normal",
+        orderType: "Pomiar moskitier",
+        address: "Wyb. Wyspiańskiego 27",
+        city: "Wrocław",
+        postCode: "50-370",
+        name: "Krysia Nowak",
+        nrtel: "500 500 500",
+        date: "01-01-2000",
+        description: "Roleta pionowa i 2 drewniane, telefon po godz. 17:00"
+
+    },
+    {
+        id: 3,
+        priority: "Normal",
+        orderType: "Pomiar żaluzji aluminiowych",
+        address: "Wyb. Wyspiańskiego 27",
+        city: "Wrocław",
+        postCode: "50-370",
+        name: "Olek Walernowicz",
+        nrtel: "500 500 500",
+        date: "01-01-2000",
+        description: "Roleta pionowa i 2 drewniane, telefon po godz. 17:00"
+
+
+    },
+    {
+        id: 4, priority: "Normal",
+        orderType: "Montaż żaluzji aluminiowych",
+        address: "Wyb. Wyspiańskiego 27",
+        city: "Wrocław",
+        postCode: "50-370",
+        name: "Aneta Kowalska",
+        nrtel: "500 500 500",
+        date: "01-01-2000",
+        description: "Roleta pionowa i 2 drewniane, telefon po godz. 17:00"
+
+
+    }
+
+]
+
+export default orders
