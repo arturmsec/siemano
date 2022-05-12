f7d8e28b94331c47d40f405ebb42a96f284b1a6c
zgredek15 <zgredek2318@gmail.com>
2022-05-12 19:56:54 +0200

    Admin panel fixed scrolling

--- vue/src/views/AdminPanelView.vue
+++ vue/src/views/AdminPanelView.vue
@@ -5,27 +5,22 @@
     </div>
     <div class="topPanel">
       Panel górny
-    </div>
-   <div class="row">
-
-      <div class="col">
-        <div class="orders"> 
+    </div>    
+   
+    <div class="orders"> 
           <OrderCard
             v-for="order in orders"
             :key="order.id"
             :order="order"
           />
-        </div>
-      </div>
-
-      <div class="col">
-        <div class="map">
+    </div> 
+    <div class="map">
           Mapa
-        </div>
-      </div>
+    </div>
+      
 
     </div>
-  </div>
+  
 
 </template>
 
@@ -48,25 +43,41 @@ export default{
 
 
 <style>
-.viewChange {
-  height: 50px;
+ .viewChange {
+  position: fixed;
+  display: inline-block;
+  z-index: 1;
+  margin-top: 40px;
+  width: 100%;
+  height: 80px;
   background: rgb(230, 207, 173);
 }
 .topPanel {
+  position: fixed;
+  z-index: 1;
+  margin-top: 120px;
+  width: 100%;
   height: 80px;
   background: lightblue;
 }
 .orders {
+  margin-top: 200px;
   height: 60vh;
   width: 30%;
   float: left;
-  background: rgb(210, 181, 181);
 }
 .map {
+  position: fixed;
+  display: inline-block;
+  margin-top: 200px;
   height: 60vh;
   width: 70%;
-  float: right;
   background: rgb(149, 199, 119);
+} 
+.wrapper{
+  position:fixed;
+  top: 0px;
+  z-index: 2;
 }
 </style>
 
